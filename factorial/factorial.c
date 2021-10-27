b794107583783ba73c276b18a1b905e8bea8cd01
Marcelo H P da Silva <marcelohenrique.backend@gmail.com>
2021-10-26 21:27:05 -0300

    Exercicio: fatorial usando os laços "for" e "while"

--- factorial/factorial.c
+++ factorial/factorial.c
@@ -17,18 +17,27 @@
 //calculates factorial using "for"
 int factorial_loop_for(int num)
 {
-    int factorial = 0;
+    int factorial = 1;
 
     for(factorial = 1; num > 1; num--){ //n-- = n = n-1
         factorial *= num; //factorial *= number; = factorial = factorial * number;
     }
     return (factorial);
 }
+
 //calculates factorial using while
 int factorial_loop_while(int num)
 {
-    printf("loading\n");
+    int factorial = 1;
+
+    while(num >= 1){
+        factorial *= num;
+        --num;
+    }
+    
+    return (factorial);
 }
+
 //two ways of calculates factorial
 int main(){
     setlocale(LC_ALL, "portuguese");
@@ -41,22 +50,23 @@ int main(){
 
     printf("Digite 1 para calcular usando o for\n");
     printf("Digite 2 para calcular usando o while\n");
-
-    printf("%d", &op);
+    scanf("%d", &op);
 
     switch (op)
     {
-    case 1:
+    case 1: //case the user read 1, call the function "..._loop_for"
         fat = factorial_loop_for(number);
         break;
-    case 2:
+    case 2: //case the user read 2, call the function "..._loop_for"
         fat = factorial_loop_while(number);
         break;
-    default:
+    default: //defalt don't call any function
         printf("Escolha inválida\n");
         break;
     }
 
+    //exit the fat of "switch case"
     printf("Resultado do fatorial: %d\n", fat);
+
     return 0;
 }
