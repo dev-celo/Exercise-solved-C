8e6f5733c60f6da0d218e5a1a3c7d9a2cc87217d
Marcelo H P da Silva <marcelohenrique.backend@gmail.com>
2021-10-26 21:13:08 -0300

    exercicio

--- factorial/factorial.c
+++ factorial/factorial.c
@@ -12,20 +12,51 @@
     date : 26-10-2021
 */
 
+
+
+//calculates factorial using "for"
+int factorial_loop_for(int num)
+{
+    int factorial = 0;
+
+    for(factorial = 1; num > 1; num--){ //n-- = n = n-1
+        factorial *= num; //factorial *= number; = factorial = factorial * number;
+    }
+    return (factorial);
+}
+//calculates factorial using while
+int factorial_loop_while(int num)
+{
+    printf("loading\n");
+}
+//two ways of calculates factorial
 int main(){
     setlocale(LC_ALL, "portuguese");
 
-    int factorial, number;
+    int number, fat, op;
 
     printf("Calculadora fatorial");
     printf("\nDigite um valor a ser calculado: ");
     scanf("%d", &number); //read value of number to be calculated
 
-    for(factorial = 1; number > 1; number--){ //n-- = n = n-1
-        factorial *= number; //factorial *= number; = factorial = factorial * number;
+    printf("Digite 1 para calcular usando o for\n");
+    printf("Digite 2 para calcular usando o while\n");
+
+    printf("%d", &op);
+
+    switch (op)
+    {
+    case 1:
+        fat = factorial_loop_for(number);
+        break;
+    case 2:
+        fat = factorial_loop_while(number);
+        break;
+    default:
+        printf("Escolha inválida\n");
+        break;
     }
 
-    printf("Resultado fatorial: %d\n", factorial);
-
+    printf("Resultado do fatorial: %d\n", fat);
     return 0;
 }
